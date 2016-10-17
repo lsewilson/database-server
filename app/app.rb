fb88579b101e7674b52677f6313e23bbce6f0c87
lsewilson <laurawilson@hotmail.co.uk>
2016-10-17 12:57:10 +0100

    connects lib files to server

--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,26 @@
 require 'sinatra/base'
+require_relative '../lib/data_store.rb'
 
 class DatabaseServer < Sinatra::Base
   get '/' do
-    'Hello DatabaseServer!'
+    erb :index
   end
+
+  get '/get' do
+    
+  end
+
+  post '/new' do
+    @store = DataStore.new
+    redirect '/get'
+  end
+
+  post '/set' do
+    @store.save_data(params[:key], params[:value])
+    redirect '/'
+  end
+
+
 
 
   # start the server if ruby file executed directly
--- spec/features/database_server_spec.rb
+++ spec/features/database_server_spec.rb
@@ -1,10 +1,19 @@
 require 'spec_helper'
 
+feature 'creating a new database server' do
+  scenario 'allows user to create a new database server' do
+    visit '/'
+    click_button "Create new database"
+    expect(current_path).to eq '/get'
+  end
+end
+
 feature 'setting keys and values' do
 
   scenario 'allows user to set keys and values' do
     visit '/set?test_key=test_value'
     expect(page.status_code).to eq 200
+    
     expect(page).to have_content 'Data stored successfully'
   end
 
@@ -14,7 +23,7 @@ feature 'getting keys and values' do
 
   before do
     visit '/set?test_key=test_value'
-  end 
+  end
 
   scenario 'allows user to retrieve keys and values' do
     visit '/'
