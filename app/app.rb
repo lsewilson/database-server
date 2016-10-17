3f8997e0a0bf199ea95db6c443704caa4fc59a3e
lsewilson <laurawilson@hotmail.co.uk>
2016-10-17 14:13:14 +0100

    replaces global variable with session

--- app/app.rb
+++ app/app.rb
@@ -2,22 +2,27 @@ require 'sinatra/base'
 require_relative '../lib/data_store.rb'
 
 class DatabaseServer < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/new' do
-    $store = DataStore.new
+    store = DataStore.new
+    session[:store] = store
     redirect '/'
   end
 
   get '/get' do
-    @value = $store.get_data(params.keys[0])
+    store = session[:store]
+    @value = store.get_data(params.keys[0])
     erb :get
   end
 
   get '/set' do
-    $store.save_data(params.keys[0], params.values[0])
+    store = session[:store]
+    store.save_data(params.keys[0], params.values[0])
     redirect '/'
   end
 
--- spec/features/database_server_spec.rb
+++ spec/features/database_server_spec.rb
@@ -10,21 +10,17 @@ end
 
 feature 'setting keys and values' do
 
-  scenario 'allows user to set keys and values' do
+  before do
+    visit '/'
+    click_button "Create new database"
+  end
+
+  scenario 'allows user to set and find keys and values' do
     visit '/set?test_key=test_value'
     expect(page.status_code).to eq 200
-  end
 
-end
-
-feature 'getting keys and values' do
-
-  before do
-    visit '/set?test_key=test_value'
-  end
-
-  scenario 'allows user to retrieve keys and values' do
     visit '/get?test_key'
     expect(page).to have_content "test_value"
   end
+
 end
