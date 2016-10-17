e11afe5834ba4f304c34a3b8f8ed4dad6f903aad
lsewilson <laurawilson@hotmail.co.uk>
2016-10-17 15:00:59 +0100

    adds test for multiple settings

--- spec/features/database_server_spec.rb
+++ spec/features/database_server_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-feature 'setting keys and values' do
+feature 'Database site' do
 
   scenario 'allows user to set and find keys and values' do
     visit '/set?test_key=test_value'
@@ -9,6 +9,19 @@ feature 'setting keys and values' do
 
     visit '/get?test_key'
     expect(page).to have_content "test_value"
+  end
+
+  scenario 'allows user to set multiple keys and values' do
+    visit '/set?first_key=first_value'
+    visit '/set?second_key=second_value'
+
+    visit '/get?first_key'
+    expect(page.status_code).to eq 200
+    expect(page).to have_content "first_value"
+
+    visit '/get?second_key'
+    expect(page.status_code).to eq 200
+    expect(page).to have_content "second_value"
   end
 
 end
